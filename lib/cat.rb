739f79c59c1e32fe30971723ab14d071faebe982
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-24 15:16:01 -0500

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,16 @@
 class Cat
   # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+  @@cats = []
+  def initialize(name, owner, mood="nervous")
+    @name = name
+    @owner = owner
+    @owner.cats << self
+    @mood = mood
+  end
+
+  def self.all
+    @@cats
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,62 @@
+require 'pry'
 class Owner
   # code goes here
+  attr_accessor :cats, :dogs
+  attr_reader :species, :name
+  @@owners = []
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@owners << self
+    @cats = []
+    @dogs = []
+    # binding.pry
+  end
+
+  def self.all
+    @@owners
+  end
+
+  def self.count
+    @@owners.uniq.count
+  end
+
+  def self.reset_all
+    @@owners.clear
+  end
+
+  def say_species
+    "I am a #{self.species}."
+  end
+
+  def buy_cat(name)
+    new_cat = Cat.new(name, self)
+    # self.cats << new_cat
+    # binding.pry
+  end
+
+  def buy_dog(name)
+    new_dog = Dog.new(name, self)
+    # self.dogs << new_dog
+    # binding.pry
+  end
+
+  def walk_dogs
+    self.dogs.each {|dog| dog.mood = "happy"}
+  end
+
+  def feed_cats
+    self.cats.each {|cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    self.dogs.concat(self.cats).each do |pet|
+      pet.mood = "nervous"
+      pet.owner = nil
+    end
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
 end
